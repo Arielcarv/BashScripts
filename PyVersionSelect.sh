85f5f294f71ab45ac7413665d85712a2d5906989
Ariel Carvalho <70777110+Arielcarv@users.noreply.github.com>
2021-11-10 18:10:42 -0300

    feat: PyVersionSelect
    A bash script to help user's select which python version installed in the system through pyenv he want's to use.
    The idea is to create an alias for the script and call it everytime the user wants to change version.
    Other than the command 'pyenv global {number of the version}', which only works if you know exactly the version number avaiable.

---
+++ PyVersionSelect.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+### A script to help the user select among the python versions installed in the system through pyenv.
+
+declare -A DictionaryOfVersions
+
+cd ~/.pyenv/versions/
+
+## Dictionary builder
+DictionaryOfVersions[1]='system'
+echo "Available Versions:"
+echo "1 - 3.8.10"
+count=2
+
+for dic in */;
+do
+	echo "$count - ${dic///}"
+    DictionaryOfVersions[$count]=${dic///}
+	((count++))
+done
+
+echo "Which version do you want?";
+read choice
+
+##
+for ((i = 0; i < $count; i++)); do
+    echo "quanto que ta: ${i}"
+    if [[ $choice = $i ]]; then
+        finalChoice=${DictionaryOfVersions[$i]}
+        pyenv global $finalChoice
+        echo "Version changed to: ${finalChoice}"
+        break
+    elif [ $choice -ge ${count} ]; then
+        ((limit=$count-1))
+        echo "That's not a valid number. Choose [1-${limit}]!"
+        echo "Which version do you want?";
+        read choice
+        i=0
+    fi
+done
