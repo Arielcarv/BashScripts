7e98e15dba6d116ed485b939528634d831484854
Ariel Carvalho <arielcarvnasc@gmail.com>
2021-11-11 01:03:30 -0300

    feat: PyenvInstallScript V2

--- PyenvInstall.sh
+++ PyenvInstall.sh
@@ -4,14 +4,13 @@ cd ~/
 
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash;
 
-
 if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
     echo "It's ZSH"
-    echo '\n### PYENV CONFIGURATION\nexport PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init --path)"\neval "$(pyenv virtualenv-init -)"' >> ./text.txt
+    printf '\n### PYENV CONFIGURATION\nexport PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init --path)"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"\n' >> ./zshrc
 fi
 if [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
     echo "It's BASH"
-    echo '\n### PYENV CONFIGURATION\nexport PATH="$HOME/.pyenv/bin:$PATH"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"' >> ./text.txt
+    printf '\n### PYENV CONFIGURATION\nexport PATH="$HOME/.pyenv/bin:$PATH"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"\n' >> ./bashrc
 fi
 
 exec $SHELL
