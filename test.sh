960c4b6c84054333e9644ce31ed3d41fc7d76719
Ariel Nascimento <ariel.nascimento@b2wdigital.com>
2021-11-11 10:36:56 -0300

    refac: PyenvInstall Update V3 + test file

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer 
 if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
     echo "It's ZSH"
     if grep -q 'eval "$(pyenv init -)"' .zshrc; then
-        echo "There were not a PATH in the ~/.zshrc file, so I'll add."
+        echo "There were not path in the ~/.zshrc file, so I'll add."
         printf '\n### PYENV CONFIGURATION\nexport PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init --path)"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"\n' >> ~/.zshrc
     fi
 fi
@@ -15,7 +15,7 @@ fi
 if [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
     echo "It's BASH"
     if ! grep -q 'eval "$(pyenv init -)"' .zshrc; then
-        echo "There were not a PATH in the ~/.bashrc file, so I'll add."
+        echo "There were not path in the ~/.bashrc file, so I'll add."
         printf '\n### PYENV CONFIGURATION\nexport PATH="$HOME/.pyenv/bin:$PATH"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"\n' >> ~/.bashrc
     fi
 fi
