4bbbc12b3ebb22bd243d8b098a1901e0178f2757
Ariel Carvalho <arielcarvnasc@gmail.com>
2022-03-03 20:19:10 -0300

    refactor: correction PyenvInstall.sh

--- PyenvInstall.sh
+++ PyenvInstall.sh
@@ -10,18 +10,18 @@ if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
     echo "It's ZSH"
     if grep -q 'eval "$(pyenv init -)"' .zshrc; then
         echo "There were not a PATH in the ~/.zshrc file, so I'll add."
-        printf '\n### PYENV CONFIGURATION\nexport PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init --path)"' >> ~/.zprofile
-        printf '\n### PYENV CONFIGURATION\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"\n' >> ~/.zshrc
+        # echo -e '\n### PYENV CONFIGURATION\nexport PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init --path)"' >> ~/.zprofile
+        echo -e '\n### PYENV CONFIGURATION\nexport PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init --path)"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"\n' >> ~/.zshrc
     fi
 fi
 
 if [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
     echo "\n\nIt's BASH"
     if ! grep -q 'eval "$(pyenv init -)"' .bashrc; then
-        echo "\n\nThere were not a PATH in the ~/.bashrc file, so I'll add."
-        sed -Ei -e '/^([^#]|$)/ {a \ export PYENV_ROOT="$HOME/.pyenv" a \ export PATH="$PYENV_ROOT/bin:$PATH" a \' -e ':a' -e '$!{n;ba};}' ~/.profile
-        printf '\n\n### PYENV CONFIGURATION\neval "$(pyenv init --path)"' >> ~/.profile
-        printf '\n\n### PYENV CONFIGURATION\nexport PATH="$HOME/.pyenv/bin:$PATH"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"\neval "$(pyenv init --path)"' >> ~/.bashrc
+        echo -e "\n\nThere were not a PATH in the ~/.bashrc file, so I'll add."
+        # sed -Ei -e '/^([^#]|$)/ {a \ export PYENV_ROOT="$HOME/.pyenv" a \ export PATH="$PYENV_ROOT/bin:$PATH" a \' -e ':a' -e '$!{n;ba};}' ~/.profile
+        # printf '\n\n### PYENV CONFIGURATION\neval "$(pyenv init --path)"' >> ~/.profile
+        echo -e'\n\n### PYENV CONFIGURATION\nexport PATH="$HOME/.pyenv/bin:$PATH"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"\neval "$(pyenv init --path)"' >> ~/.bashrc
     fi
 fi
 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # BashScripts
 Several bash scripts for useful things.
+
+-  PyenvInstall
+
+```bash
+chmod +x PyenvInstall.sh
+./PyenvInstall.sh
+```
