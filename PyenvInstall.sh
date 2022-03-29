ae907050a57fdebf7d83ed1f50d742067b39038f
Ariel Carvalho <70777110+Arielcarv@users.noreply.github.com>
2022-03-29 17:32:58 -0300

    refactor: dependencies place

--- PyenvInstall.sh
+++ PyenvInstall.sh
@@ -2,6 +2,18 @@
 
 cd ~/
 
+## Verify if user is SU or not
+SUDO=''
+if [ "$EUID" -ne 0 ]; then
+	SUDO='sudo'
+fi
+
+## Install dependencies for python versions
+$SUDO apt update -qq; 
+$SUDO apt install -qq -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
 ## Download and install Pyenv
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash;
 
@@ -24,16 +36,6 @@ if [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
         echo -e '\n\n### PYENV CONFIGURATION\nexport PATH="$HOME/.pyenv/bin:$PATH"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"\neval "$(pyenv init --path)"' >> ~/.bashrc
     fi
 fi
-
-## Install dependencies for python versions
-SUDO=''
-if [ "$EUID" -ne 0 ]; then
-	SUDO='sudo'
-fi
-
-$SUDO apt update -qq; 
-$SUDO apt install -qq -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
 
 ## Final message
 echo -e '\n\n\033[0;36m Log-out/Log-in for the changes to take action! \033[0m\n'
