fba6308b5e8cd15aad88af9792910d8ae0d13ed9
Ariel Carvalho Nascimento <arielcarvnasc@gmail.com>
2022-09-09 23:19:58 -0300

    feat: add post-installation script

---
+++ post-installation-ubuntu.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+## Removendo travas eventuais do apt ##
+
+sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ;
+
+## Atualizando o repositório ##
+
+sudo apt update &&
+
+## Instalando pacotes e programas do repositório deb do Ubuntu ##
+
+sudo apt install python3 python-pip docker docker-compose git build-essential gcc libssl-dev curl zsh -y
+
+## Softwares que precisam de download externo ##
+cd ~/Downloads/
+
+wget --content-disposition "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i *.deb
+
+## Install Spotify
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+## Install Sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+sudo apt-get update
+sudo apt-get install spotify-client sublime-text -y
+
+
+## Install fonts
+wget https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip
+unzip Fira_Code_v6.2.zip -d ./Fira_Code_v6.2
+cp ./Fira_Code_v6.2/ttf/* ~/.local/share/fonts/
+rm -r ./Fira_Code_v6.2/ ./Fira_Code_v6.2.zip 
+
+wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold%20Italic.ttf https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold.ttf https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Italic.ttf https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Regular.ttf
+cp ./MesloLGS* ~/.local/share/fonts/
+rm ./MesloLGS*
+
+## Install Oh-My-ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## Install Oh-My-ZSH Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+## Install PowerLevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i 's/ZSH_THEME=".*"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/' ~/.zshrc
+
+## Install Pyenv
+git clone git@github.com:Arielcarv/BashScripts.git
+bash ./BashScripts/PyenvInstall.sh
+
+
+## Atualização do sistema ##
+
+# sudo apt update && sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y &&
+
+#Fim do Script ##
+
+echo "Finalizado"
